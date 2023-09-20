d180fe4468d168c891990e9981a2e20217eeae00
aditikashyap914 <aditikashyap914@gmail.com>
2023-09-20 17:47:16 +0530

    new module pay-slip is created

---
+++ client/src/components_update/PaySlip/PaySlip.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const PaySlip = () => {
+  return (
+    <div>
+      <Grid>
+        <Box sx={{ display: "flex", marginLeft: "60px", marginRight: "60px" }}>
+          Payment Slip
+        </Box>
+      </Grid>
+    </div>
+  );
+};
+
+export default PaySlip;
