901e435d4e22c77cc3582e37884b544324bbc4ad
aditikashyap914 <aditikashyap914@gmail.com>
2023-09-28 13:13:16 +0530

    some changes updated in pay-slip

--- client/src/components_update/PaySlip/PaySlip.js
+++ client/src/components_update/PaySlip/PaySlip.js
@@ -1,21 +1,55 @@
 import React from "react";
-import { FormControl, Grid, Box, Typography, Card } from "@mui/material";
+import {
+  Grid,
+  Box,
+  Typography,
+  Paper,
+  Divider,
+  Input,
+  TextField,
+} from "@mui/material";
 
 const PaySlip = () => {
   return (
     <div>
       <Grid>
-        <FormControl>
-          <Card
-            sx={{
-              display: "flex",
-              bgcolor: "grey",
-              boxShadow: "5px",
-            }}
-          >
-            <Typography>Payment Slip</Typography>
-          </Card>
-        </FormControl>
+        <Paper
+          sx={{
+            display: "flex",
+            bgcolor: "background.paper",
+            boxShadow: "5px",
+            marginLeft: "200px",
+            marginRight: "200px",
+            marginTop: "10px",
+          }}
+        >
+          <Grid>
+            <Grid>
+              <Typography sx={{ alignItems: "center" }}>Salary Slip</Typography>
+            </Grid>
+            <Divider orientation="horizontal" />
+            <Grid>
+              <TextField
+                id="standard-basic"
+                label="Employee Id"
+                variant="standard"
+              />
+            </Grid>
+            <Grid>
+              <TextField
+                id="standard-basic"
+                label="Employee Name"
+                variant="standard"
+              />
+
+              <TextField
+                id="standard-basic"
+                label="last Name"
+                variant="standard"
+              />
+            </Grid>
+          </Grid>
+        </Paper>
       </Grid>
     </div>
   );
