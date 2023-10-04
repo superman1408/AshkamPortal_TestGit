dee3d1673dc8b7626c1192a47334193abeaaea20
aditikashyap914 <aditikashyap914@gmail.com>
2023-10-04 11:44:47 +0530

    Some changes updated

--- client/src/components_update/PaySlip/PaySlip.js
+++ client/src/components_update/PaySlip/PaySlip.js
@@ -44,75 +44,62 @@ const PaySlip = () => {
             marginBottom: "30px",
           }}
         >
-          <Grid>
+          <Typography
+            variant="h4"
+            sx={{
+              // flexGrow: 1,
+              fontSize: "30px",
+              fontWeight: "bold",
+              marginTop: "20px",
+              marginBottom: "10px",
+            }}
+          >
+            Salary Slip
+          </Typography>
+
+          <Divider
+            sx={{
+              borderWidth: "2px",
+            }}
+          />
+
+          <Typography sx={{ marginTop: "20px", marginBottom: "0px" }}>
+            Employee Pay Summary
+          </Typography>
+
+          <Divider orientation="horizontal" color="grey" />
+
+          <TextField
+            sx={{ display: "flex", mt: "10px" }}
+            margin="normal"
+            type="text"
+            name="employeeId"
+            label="Employee Id"
+            variant="outlined"
+            required
+          />
+
+          <Grid sx={{ display: "flex", flexDirection: "row" }}>
             <Grid>
-              <Typography
-                variant="h4"
-                sx={{
-                  // flexGrow: 1,
-                  fontSize: "30px",
-                  fontWeight: "bold",
-                  marginTop: "20px",
-                  marginBottom: "10px",
-                }}
-              >
-                Salary Slip
-              </Typography>
+              <TextField
+                sx={{ display: "flex", mr: "20px" }}
+                type="text"
+                margin="normal"
+                name="firstName"
+                // id="standard-basic"
+                label="First Name"
+                variant="outlined"
+                required
+              />
             </Grid>
-            <Divider
-              sx={{
-                borderWidth: "2px",
-              }}
-            />
 
-            <Typography sx={{ marginTop: "20px", marginBottom: "0px" }}>
-              Employee Pay Summary
-            </Typography>
-
-            <Divider orientation="horizontal" color="grey" />
-
-            <TextField
-              sx={{ display: "flex", mt: "10px" }}
-              type="text"
-              name="employeeId"
-              label="Employee Id"
-              variant="outlined"
-              fullWidth
-              required
-            />
-            <Grid sx={{ display: "flex", flexDirection: "row" }}>
-              <Grid>
-                <TextField
-                  sx={{ display: "flex", mr: "20px" }}
-                  type="text"
-                  name="firstName"
-                  // id="standard-basic"
-                  label="First Name"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-
-              <Grid>
-                <TextField
-                  type="text"
-                  name="lastName"
-                  label="Last Name"
-                  variant="outlined"
-                  fullWidth
-                  required
-                />
-              </Grid>
-            </Grid>
             <Grid>
               <TextField
                 type="text"
+                margin="normal"
                 name="lastName"
-                // id="standard-basic"
-                label="last Name"
+                label="Last Name"
                 variant="outlined"
-                fullWidth
                 required
               />
             </Grid>
@@ -120,40 +107,219 @@ const PaySlip = () => {
 
           <TextField
             type="text"
+            margin="normal"
+            name="lastName"
+            // id="standard-basic"
+            label="last Name"
+            variant="outlined"
+            required
+          />
+
+          <TextField
+            type="text"
+            margin="normal"
             name="UAN"
             // id="standard-basic"
             label="UAN No."
             variant="outlined"
-            fullWidth
             required
           />
+
           <TextField
             type="text"
+            margin="normal"
             name="payDays"
             // id="standard-basic"
             label="Pay Days"
             variant="outlined"
-            fullWidth
             required
           />
           <TextField
             type="text"
+            margin="normal"
             name="payPeriod"
             // id="standard-basic"
             label="payPeriod"
             variant="outlined"
-            fullWidth
             required
           />
           <TextField
             type="text"
+            margin="normal"
             name="payDate"
             // id="standard-basic"
             label="payDate"
             variant="outlined"
-            fullWidth
             required
           />
+
+          <Typography sx={{ marginTop: "50px", marginBottom: "0px" }}>
+            Income Details
+          </Typography>
+
+          <Divider orientation="horizontal" color="grey" />
+
+          <Grid sx={{ display: "flex", flexDirection: "row", mb: "10px" }}>
+            <Grid sx={{ display: "flex", flexDirection: "column" }}>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography>Earnings</Typography>
+                <Typography marginLeft={30}>Amount</Typography>
+              </Grid>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={4} marginRight={30}>
+                  Basic
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="payDate"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={15}>
+                  House rent allowance
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  Conveyance allowance
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  Communication
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  Uniform allowance
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  Medical allowance
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  City Factors
+                </Typography>
+                <TextField
+                  size="small"
+                  type="text"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={3} marginRight={14}>
+                  Gross Earnings (Rs)
+                </Typography>
+                <TextField
+                  size="small"
+                  type="text"
+                  margin="normal"
+                  name="amount"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+
+            <Grid sx={{ display: "flex", flexDirection: "column", ml: "80px" }}>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography>Deductions</Typography>
+                <Typography marginLeft={30}>Amount</Typography>
+              </Grid>
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={4} marginRight={20}>
+                  Income Tax
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="payDate"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+
+              <Grid sx={{ display: "flex", flexDirection: "row" }}>
+                <Typography marginTop={4} marginRight={17}>
+                  Provident fund
+                </Typography>
+                <TextField
+                  type="text"
+                  size="small"
+                  margin="normal"
+                  name="payDate"
+                  // id="standard-basic"
+                  label="amount"
+                  variant="outlined"
+                />
+              </Grid>
+            </Grid>
+          </Grid>
         </Grid>
       </Paper>
     </div>
