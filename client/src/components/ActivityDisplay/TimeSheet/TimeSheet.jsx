eb3d3a1d0b92007337b8d5935fcc207c5b450b3f
superman1408 <superman14082017@gmail.com>
2024-01-18 18:10:21 +0530

    WOrking on time sheet..git add -A

--- client/src/components/ActivityDisplay/TimeSheet/TimeSheet.jsx
+++ client/src/components/ActivityDisplay/TimeSheet/TimeSheet.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 // import TimeSheetForm from '../TimeSheetForm/TimeSheetForm';
 // import { getPosts, getPost } from '../../../action/posts';
-
+import { Button, Card, Typography, Avatar } from "@mui/material";
 // const time = [{employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}];
 
 const TimeSheet = ({post,currentId}) => {
@@ -23,9 +23,31 @@ const TimeSheet = ({post,currentId}) => {
     <div>
         {
           post.jobCode.map((item, i) => (
-            <li key={i}>
-              {item}
-            </li>
+            <Card
+              elevation={10}
+              sx={{
+                padding: "5px",
+                width: "auto",
+                margin: "10px",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                bgcolor: "#aee3e8",
+              }}
+            >
+              <div key={i} className="translate-x-[42%]">
+                <Typography
+                  variant="h6"
+                  sx={{
+                    textAlign: "center",
+                    padding: "5px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {item}
+                </Typography>
+              </div>
+            </Card>
           ))
         }
     </div>
