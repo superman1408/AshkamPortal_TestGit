266b1f925ac1392247b6f84c36b3479b2715402e
superman1408 <superman14082017@gmail.com>
2024-01-19 16:29:34 +0530

    Some changes in time sheet..git add --all

--- client/src/components/ActivityDisplay/TimeSheet/TimeSheet.jsx
+++ client/src/components/ActivityDisplay/TimeSheet/TimeSheet.jsx
@@ -1,24 +1,9 @@
 import React from 'react';
-// import { useDispatch, useSelector } from 'react-redux';
 
-// import TimeSheetForm from '../TimeSheetForm/TimeSheetForm';
-// import { getPosts, getPost } from '../../../action/posts';
-import { Button, Card, Typography, Avatar } from "@mui/material";
-// const time = [{employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}, {employeeId: "12345", jobCode: "AEPL-002-PL-001", start: "9th Jan 2024", hoursWorked: "5", end:"12th Jan 2024"}];
+import { Card, Typography} from "@mui/material";
+
 
 const TimeSheet = ({post,currentId}) => {
-  // console.log(currentId);
-  // const  timeSheets  = useSelector((state) => state.posts);
-  // const [sheets, setSheets] = useState({posts})
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   if (post) {
-  //     dispatch(getPost(currentId));
-  //   }
-  // },[post, dispatch, timeSheets, currentId])
-
-  // console.log(post);
 
   const array = [];
   for (let i = 0; i < post.jobCode.length; i++) {
@@ -31,7 +16,6 @@ const TimeSheet = ({post,currentId}) => {
     
   };
 
-  console.log(array);
 
 
 
