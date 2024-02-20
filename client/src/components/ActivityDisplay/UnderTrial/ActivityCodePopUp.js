46bee392932187fdc307d499b124506d2c26a890
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-02-20 15:38:14 +0530

    Activity Code is added

--- client/src/components/ActivityDisplay/UnderTrial/ActivityCodePopUp.js
+++ client/src/components/ActivityDisplay/UnderTrial/ActivityCodePopUp.js
@@ -4,8 +4,11 @@ const ActivityCodePopUp = ({ setActivityCode, setActivityOpen }) => {
   const [disciplineCode, setDisciplineCode] = useState();
   const [discipline, setDiscipline] = useState();
 
+  const [subActivityCode, setSubActivityCode] = useState();
+  const [subActivity, setSubActivity] = useState();
+
   const appendData = () => {
-    const updatedList = disciplineCode; // Combine disciplineCode and year
+    const updatedList = disciplineCode + subActivityCode; // Combine disciplineCode and year
     setActivityCode(updatedList);
     console.log(updatedList);
     setActivityOpen(false);
@@ -14,6 +17,8 @@ const ActivityCodePopUp = ({ setActivityCode, setActivityOpen }) => {
   const closetoggle = () => {
     setActivityOpen(false);
   };
+
+  //  ---------------Discipline Code Function -------------------------------
 
   const handleDisciplineChange = (event) => {
     const selectedDiscipline = event.target.value;
@@ -49,6 +54,86 @@ const ActivityCodePopUp = ({ setActivityCode, setActivityOpen }) => {
     };
 
     return Disciplines[DisciplineName];
+  };
+
+  useEffect(() => {
+    console.log(discipline);
+  }, [discipline]);
+
+  //  ---------------Sub  Activity Code Function -------------------------------
+
+  const handleSubActivityChange = (event) => {
+    const selectedSubActivity = event.target.value;
+    const selectedSubActivityCode = getSubActivityCode(selectedSubActivity); // Convert month name to month number
+    setSubActivity(selectedSubActivity);
+    setSubActivityCode(selectedSubActivityCode); // Update month state with the month number
+    console.log("selectedDisciplineCoden", selectedSubActivity);
+  };
+
+  // Function to convert month name to its number
+  const getSubActivityCode = (SubActivityName) => {
+    const SubActivityies = {
+      Select: "--",
+      Airflow: "001",
+      CableSchedule: "002",
+      CableSizing: "003",
+      CableTrayORConduit: "004",
+      Coordination: "005",
+      CoordinationORSupervision: "006",
+      DataSheets: "007",
+      DocReviews: "008",
+      DocumentIssue: "009",
+      DocumentReciept: "010",
+      DocumentReporting: "011",
+      Duct: "012",
+      Earthing: "013",
+      EquipmentLayout: "014",
+      EquipmentSizing: "015",
+      Equipments: "016",
+      ExternalMeetings: "017",
+      FEA: "018",
+      Foundation: "019",
+      FoundationLayout: "020",
+      HAZ: "021",
+      HeatandMassBalance: "022",
+      I_OR_Olist: "023",
+      InstrumentLayout: "024",
+      InstrumentList: "025",
+      Instruments: "026",
+      InterfaceSchedule: "027",
+      InternalMeetings: "028",
+      InvoiceInput: "029",
+      layout: "030",
+      Lighting: "031",
+      LightningProtection: "032",
+      LineList: "033",
+      LineSizing: "034",
+      LoadList: "035",
+      LoopDrawings: "036",
+      Meetings: "037",
+      Mooring: "038",
+      MooringLines: "039",
+      MTO: "040",
+      P_OR_ID: "041",
+      PFD: "042",
+      PipeStress: "043",
+      PipingLayout: "044",
+      Planning: "045",
+      PowerSystemCalculations: "046",
+      ProjectSupport: "047",
+      Proposals: "048",
+      PumpSizing: "049",
+      Reporting: "050",
+      SchematicDrawings: "051",
+      SingleLineDrawings: "052",
+      Specifications: "053",
+      Structural: "054",
+      StructuralDrawings: "054",
+      Training: "056",
+      ValveSizing: "057",
+    };
+
+    return SubActivityies[SubActivityName];
   };
 
   useEffect(() => {
@@ -108,6 +193,95 @@ const ActivityCodePopUp = ({ setActivityCode, setActivityOpen }) => {
                 <option value="Procurement">Procurement</option>
                 <option value="Projects">Projects</option>
                 <option value="Telecom">Telecom</option>
+              </select>
+            </div>
+            <div>
+              {/*_______________Discipline Code____________________  */}
+
+              <label
+                style={{
+                  marginTop: "20px",
+                }}
+              >
+                Sub Activities
+              </label>
+              <select
+                style={{
+                  width: "200px",
+                  height: "30px",
+                  fontSize: "16px",
+                  marginTop: "10px",
+                }}
+                name="Discipline"
+                value={subActivity}
+                onChange={handleSubActivityChange}
+              >
+                <option value="select">Select</option>
+                <option value="Airflow">Air flow</option>
+                <option value="CableSchedule">Cable Schedule</option>
+                <option value="CableSizing">Cable Sizing</option>
+                <option value="CableTrayORConduit">Cable Tray/Conduit</option>
+                <option value="Coordination">Coordination</option>
+                <option value="CoordinationORSupervision">
+                  Coordination/ supervision
+                </option>
+                <option value="DataSheets">Data Sheets</option>
+                <option value="DocReviews">Doc Reviews</option>
+                <option value="DocumentIssue">Document Issue</option>
+                <option value="DocumentReciept">Document Reciept</option>
+                <option value="DocumentReporting">Document Reporting</option>
+                <option value="Duct">Duct</option>
+                <option value="Earthing">Earthing</option>
+                <option value="EquipmentLayout">Equipment Layout</option>
+                <option value="EquipmentSizing">Equipment Sizing</option>
+                <option value="Equipments">Equipments</option>
+                <option value="ExternalMeetings">External Meetings</option>
+                <option value="FEA">FEA</option>
+                <option value="Foundation">Foundation</option>
+                <option value="FoundationLayout">Foundation Layout</option>
+                <option value="HAZ">HAZ</option>
+                <option value="HeatandMassBalance">
+                  Heat and Mass Balance
+                </option>
+                <option value="I_OR_Olist">I/Olist</option>
+                <option value="InstrumentLayout">Instrument Layout</option>
+                <option value="InstrumentList">Instrument List</option>
+                <option value="Instruments">Instruments</option>
+                <option value="InterfaceSchedule">Interface Schedule</option>
+                <option value="InternalMeetings">Internal Meetings</option>
+                <option value="InvoiceInput">Invoice Input</option>
+                <option value="layout">layout</option>
+                <option value="Lighting">Lighting</option>
+                <option value="LightningProtection">
+                  Lightning Protection
+                </option>
+                <option value="LineList">Line List</option>
+                <option value="LineSizing">Line Sizing</option>
+                <option value="LoadList">Load List</option>
+                <option value="LoopDrawings">Loop Drawings</option>
+                <option value="Meetings">Meetings</option>
+                <option value="Mooring">Mooring</option>
+                <option value="MooringLines">Mooring Lines</option>
+                <option value="MTO">MTO</option>
+                <option value="P_OR_ID">P/ID</option>
+                <option value="PFD">PFD</option>
+                <option value="PipeStress">Pipe Stress</option>
+                <option value="PipingLayout">Piping Layout</option>
+                <option value="Planning">Planning</option>
+                <option value="PowerSystemCalculations">
+                  Power System Calculations
+                </option>
+                <option value="ProjectSupport">Project Support</option>
+                <option value="Proposals">Proposals</option>
+                <option value="PumpSizing">Pump Sizing</option>
+                <option value="Reporting">Reporting</option>
+                <option value="SchematicDrawings">Schematic Drawings</option>
+                <option value="SingleLineDrawings">Single Line Drawings</option>
+                <option value="Specifications">Specifications</option>
+                <option value="Structural">Structural</option>
+                <option value="StructuralDrawings">Structural Drawings</option>
+                <option value="Training">Training</option>
+                <option value="ValveSizing">Valve Sizing</option>
               </select>
             </div>
           </div>
