bca2663b238c84fd63853dec8d63c89f58013662
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-02-23 16:24:01 +0530

    Data is punched in Data Base

--- client/src/components/ActivityDisplay/UnderTrial/Evolve.js
+++ client/src/components/ActivityDisplay/UnderTrial/Evolve.js
@@ -5,23 +5,19 @@ import { useDispatch } from "react-redux";
 
 import "./Style1.css"; // Import CSS file for styling
 import { todoList } from "../../../api";
+// import ProjectCode from "./ProjectCodePopUp";
 import ProjectCodePopUp from "./ProjectCodePopUp";
 import ActivityCodePopUp from "./ActivityCodePopUp";
-const Evolve = ({ currentId, posts }) => {
+const Evolve = ({ currentId }) => {
   const dispatch = useDispatch();
   const [entries, setEntries] = useState([]);
   const [projectCode, setProjectCode] = useState("");
   const [activityCode, setActivityCode] = useState("");
-  const [date, setDate] = useState(" ");
-  const [netTime, setNetTime] = useState(" ");
-  const [overTime, setOverTime] = useState(" ");
+  const [date, setDate] = useState("");
+  const [netTime, setNetTime] = useState("");
+  const [overTime, setOverTime] = useState("");
   const [editIndex, setEditIndex] = useState(-1);
-
-  // for selected activity code
-
-  const [selectedDiscipline, setSelectedDiscipline] = useState("");
-
-  const [punchedDateTime, setPunchedDateTime] = useState(null);
+  const [showAnalysis, setShowAnalysis] = useState(false);
 
   // useEffect(() => {
   //   calculateAnalytics(); // Call calculateAnalytics whenever entries are updated
@@ -35,8 +31,16 @@ const Evolve = ({ currentId, posts }) => {
       date,
       netTime: parseFloat(netTime),
       overTime: parseFloat(overTime),
-      punchedDateTime,
     };
+    // const formData = {
+    //   projectcode: [...newEntry, projectCode],
+    //   activitycode: [...newEntry, activityCode],
+    //   date1: [...newEntry, date],
+    //   nettime: [...newEntry, netTime],
+    //   overtime: [...newEntry, overTime],
+    // }
+
+    // console.log(newEntry)
 
     if (validateEntry(newEntry)) {
       if (editIndex !== -1) {
@@ -46,13 +50,9 @@ const Evolve = ({ currentId, posts }) => {
         console.log(updatedEntries);
         // await dispatch(todoList(newEntry, currentId));
         setEditIndex(-1); // Reset edit index
-        clearForm();
-        handlePunch();
       } else {
         setEntries([...entries, newEntry]);
         // console.log(entries)
-        clearForm();
-        handlePunch();
         await dispatch(todoList(newEntry, currentId));
       }
     } else {
@@ -61,7 +61,6 @@ const Evolve = ({ currentId, posts }) => {
       );
     }
     clearForm();
-    handlePunch();
   };
 
 
@@ -140,11 +139,20 @@ const Evolve = ({ currentId, posts }) => {
     setActivityOpen(!activityopen);
   };
 
-  const handlePunch = () => {
-    const currentDate = new Date();
-    const formattedDate = currentDate.toLocaleString(); // Format the date as needed
+  // const handleInputChange = (event) => {
+  //   setProjectCode(event.target.value);
+  // };
 
-    setPunchedDateTime(formattedDate);
+  const handleAnalysis = () => {
+    if (showAnalysis === false) {
+      setShowAnalysis(true);
+    } else {
+      setShowAnalysis(false);
+    }
+
+    // if (entries === 0) {
+    //   setShowAnalysis(false);
+    // }
   };
 
 
@@ -186,7 +194,7 @@ const Evolve = ({ currentId, posts }) => {
                 }}
                 type="text"
                 id="projectCode"
-                value={projectCode}
+                defaultValue={projectCode}
                 onFocus={togglePopup1} // Using onFocus event to trigger the popup
                 // onChange={handleInputChange} // Handle input change
                 autoComplete="off"
@@ -218,7 +226,7 @@ const Evolve = ({ currentId, posts }) => {
                 }}
                 type="text"
                 id="activityCode"
-                value={activityCode}
+                defaultValue={activityCode}
                 // onChange={(e) => setActivityCode(e.target.value)}
                 onFocus={togglePopup2}
                 autoComplete="off"
@@ -227,8 +235,6 @@ const Evolve = ({ currentId, posts }) => {
                 <ActivityCodePopUp
                   setActivityCode={setActivityCode}
                   setActivityOpen={setActivityOpen}
-                  selectedDiscipline={selectedDiscipline}
-                  setSelectedDiscipline={setSelectedDiscipline}
                 />
               )}
             </div>
@@ -240,19 +246,9 @@ const Evolve = ({ currentId, posts }) => {
                 Date:
               </label>
               <input
-                style={{
-                  width: "100%",
-
-                  height: "30px",
-                  padding: "8px",
-                  fontSize: "16px",
-                  border: "1px solid #ccc",
-                  borderRadius: "4px",
-                  color: "#e55d17",
-                }}
                 type="date"
                 id="date"
-                value={date}
+                defaultValue={date}
                 onChange={(e) => setDate(e.target.value)}
               />
             </div>
@@ -263,7 +259,7 @@ const Evolve = ({ currentId, posts }) => {
               <input
                 type="number"
                 id="netTime"
-                value={netTime}
+                defaultValue={netTime}
                 onChange={(e) => setNetTime(e.target.value)}
               />
             </div>
@@ -275,7 +271,7 @@ const Evolve = ({ currentId, posts }) => {
               <input
                 type="number"
                 id="overTime"
-                value={overTime}
+                defaultValue={overTime}
                 onChange={(e) => setOverTime(e.target.value)}
               />
             </div>
@@ -321,7 +317,6 @@ const Evolve = ({ currentId, posts }) => {
                     >
                       <button onClick={() => editEntry(index)}>Edit</button>
                       <button onClick={() => deleteEntry(index)}>Delete</button>
-                      <span>{entry.punchedDateTime}</span>
                     </div>
                   </tr>
                 ))}
