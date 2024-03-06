dfb70f285f114d94bb08eca24a6c5e3a58ddd9af
aditikashyap914 <aditikashyap914@gmail.com>
2024-03-06 11:44:45 +0530

    changes updated

--- client/src/components/PrinttingLayout/PrintingLayout.js
+++ client/src/components/PrinttingLayout/PrintingLayout.js
@@ -19,21 +19,13 @@ import jsPDF from "jspdf";
 
 import AshkamLogo from "../../assets/AshKamLogo.png";
 
+import PaySlip from "../Payslip/PaySlip";
+
 const PrintingLayout = () => {
   const user = JSON.parse(localStorage.getItem("profile"));
   const [isPrinting, setIsPrinting] = useState(false);
 
-  function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
-  }
-
-  const rows = [
-    createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-    createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-    createData("Eclair", 262, 16.0, 24, 6.0),
-    createData("Cupcake", 305, 3.7, 67, 4.3),
-    createData("Gingerbread", 356, 16.0, 49, 3.9),
-  ];
+  const [totalInModule2, setTotalInModule2] = useState(0);
 
   const pdfRef = useRef();
   const downloadPdf = () => {
