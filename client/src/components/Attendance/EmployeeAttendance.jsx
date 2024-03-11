5eb8521d26c67b744ef40940f492d63219111232
superman1408 <superman14082017@gmail.com>
2024-03-11 13:18:57 +0530

    excel sheet is reading

--- client/src/components/Attendance/EmployeeAttendance.jsx
+++ client/src/components/Attendance/EmployeeAttendance.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import Excel from 'exceljs';
 
 const EmployeeAttendance = () => {
-  return <div>Employee Attendance</div>;
+
+    const handleChange = (e) => {
+      const file = e.target.files[0];
+      const wb = new Excel.Workbook();
+      const reader = new FileReader();
+
+      reader.readAsArrayBuffer(file);
+      reader.onload = () => {
+          const buffer = reader.result;
+          wb.xlsx.load(buffer).then((workbook) => {
+              console.log(workbook, 'workbook instance');
+              workbook.eachSheet((sheet, id) => {
+                  sheet.eachRow((row, rowIndex) => {
+                      console.log(row.values, rowIndex);
+                  });
+              });
+          });
+      };
+  };
+
+  return (
+    <div className="card">
+      <div>Upload Excel File</div>
+      <input type='file' onChange={(e) => handleChange(e)} />
+    </div>
+  );
 };
 
 export default EmployeeAttendance;
