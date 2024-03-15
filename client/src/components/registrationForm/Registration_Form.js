71b54e00843048b04368b6aba90c6024d36fa237
superman1408 <superman14082017@gmail.com>
2024-03-15 10:03:00 +0530

    Registration is working properly..git add --all!

--- client/src/components/registrationForm/Registration_Form.js
+++ client/src/components/registrationForm/Registration_Form.js
@@ -94,6 +94,7 @@ const RegistrationForm = () => {
               relationship: items.relationship,
               selectedFile: items.selectedFile,
             }));
+            break;
           }
         }
       })
