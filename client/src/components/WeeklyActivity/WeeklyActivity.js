fd36f74e6af9f345d8b4bea20442054dae1d82a0
aditikashyap914 <aditikashyap914@gmail.com>
2024-03-20 17:36:17 +0530

    merging

--- client/src/components/WeeklyActivity/WeeklyActivity.js
+++ client/src/components/WeeklyActivity/WeeklyActivity.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 // import { useNavigate } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
 import { CChart } from "@coreui/react-chartjs";
-import { Box, Typography, Grid, ButtonBase, IconButton } from "@mui/material";
+
 import { Box, Typography, Grid, IconButton } from "@mui/material";
 
 import DescriptionIcon from "@mui/icons-material/Description";
