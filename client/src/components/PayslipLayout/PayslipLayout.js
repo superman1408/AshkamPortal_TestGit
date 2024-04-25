3707c41c16bac9b7514268f187f035a571117aeb
Farheen Kaifee <farheenkaifee555@gmail.com>
2024-04-25 12:07:51 +0530

    Payslip layout

--- client/src/components/PayslipLayout/PayslipLayout.js
+++ client/src/components/PayslipLayout/PayslipLayout.js
@@ -1,7 +1,23 @@
 import { Button, Card, Typography } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 const PayslipLayout = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (e) => {
+    setSelectedFile(e.target.files[0]);
+  };
+
+  const handleUpload = () => {
+    // Handle file upload logic here
+    if (selectedFile) {
+      // You can use APIs like FileReader to read the file content or
+      // use it to upload the file to a server
+      console.log("Uploading file:", selectedFile);
+    } else {
+      console.log("No file selected.");
+    }
+  };
   return (
     <>
       <div style={{ alignItems: "center" }}>
@@ -12,7 +28,8 @@ const PayslipLayout = () => {
             </div>
             <div style={{ display: "flex" }}>
               <Typography>file Upload</Typography>
-              <Button>upload</Button>{" "}
+              <input type="file" onChange={handleFileChange} accept=".pdf" />
+              <Button onClick={handleUpload}>upload</Button>{" "}
             </div>
           </div>
         </Card>
