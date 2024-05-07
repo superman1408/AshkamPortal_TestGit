8c117259a2fc6e514340d67863737acdf70fb99f
aditikashyap914 <aditikashyap914@gmail.com>
2024-05-07 15:51:07 +0530

    payslip download module created

--- client/src/components/PayslipLayout/Uploading.js
+++ client/src/components/PayslipLayout/Uploading.js
@@ -60,106 +60,50 @@ const Uploading = () => {
 
   return (
     <>
-      <Grid sx={{ display: "flex", flexDirection: "column" }}>
-        <Grid>
-          <Card sx={{ textAlign: "center", margin: "50px 0px 50px 100px" }}>
-            <Grid
-              container
-              sx={{
-                padding: "30px",
-                display: "flex",
-                width: "auto",
-                justifyContent: "space-evenly",
+      <Card sx={{ textAlign: "center", margin: "50px 0px 50px 100px" }}>
+        <Grid
+          container
+          sx={{
+            padding: "30px",
+            display: "flex",
+            width: "auto",
+            justifyContent: "space-evenly",
+          }}
+        >
+          <Grid item sx={{ marginLeft: "20px" }}>
+            <ComboBox posts={posts} setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item sx={{ display: "flex", marginLeft: "100px" }}>
+            <Typography sx={{ fontWeight: "bold", marginTop: "10px" }}>
+              File Upload
+            </Typography>
+            <div
+              style={{
+                fontWeight: "bold",
+                marginTop: "5px",
+                marginLeft: "10px",
               }}
             >
-              <Grid item sx={{ marginLeft: "20px" }}>
-                <ComboBox posts={posts} setCurrentId={setCurrentId} />
-              </Grid>
-              <Grid item sx={{ display: "flex", marginLeft: "100px" }}>
-                <Typography sx={{ fontWeight: "bold", marginTop: "10px" }}>
-                  File Upload
-                </Typography>
-                <div
-                  style={{
-                    fontWeight: "bold",
-                    marginTop: "5px",
-                    marginLeft: "10px",
-                  }}
-                >
-                  <input
-                    type="text"
-                    onChange={handleTitleChange}
-                    placeholder="Enter the Title"
-                  />
-                  <input
-                    style={{ marginLeft: "50px" }}
-                    type="file"
-                    onChange={handleFileChange}
-                    accept=".pdf"
-                  />
-                </div>
-              </Grid>
-              <Grid item sx={{ marginLeft: "0px" }}>
-                <Button onClick={handleUpload}>
-                  upload <FileUploadIcon />
-                </Button>
-              </Grid>
-            </Grid>
-          </Card>
-        </Grid>
-        <Grid sx={{ display: "flex" }}>
-          <Grid>
-            <div>
-              <table
-                className="time-sheet-table"
-                style={{
-                  padding: "15px",
-                  // backgroundColor: "#f2f2f2",
-                  borderCollapse: "collapse",
-                  border: "1px solid black",
-                  marginLeft: "70px",
-                  marginRight: "auto",
-                  borderSpacing: "0",
-                  width: "100%",
-                  marginBottom: "10px",
-                  maxWidth: "500px",
-                }}
-              >
-                <thead>
-                  <tr>
-                    <th>abc</th>
-                  </tr>
-                </thead>
-              </table>
+              <input
+                type="text"
+                onChange={handleTitleChange}
+                placeholder="Enter the Title"
+              />
+              <input
+                style={{ marginLeft: "50px" }}
+                type="file"
+                onChange={handleFileChange}
+                accept=".pdf"
+              />
             </div>
           </Grid>
-          <Grid>
-            <div>
-              <table
-                className="time-sheet-table"
-                style={{
-                  padding: "15px",
-                  // backgroundColor: "#f2f2f2",
-                  borderCollapse: "collapse",
-                  border: "1px solid black",
-                  marginLeft: "auto",
-                  marginRight: "auto",
-                  borderSpacing: "0",
-                  width: "100%",
-                  marginBottom: "10px",
-                  maxWidth: "800px",
-                }}
-              >
-                <thead>
-                  <tr>
-                    <th>abc</th>
-                  </tr>
-                </thead>
-              </table>
-            </div>
+          <Grid item sx={{ marginLeft: "0px" }}>
+            <Button onClick={handleUpload}>
+              upload <FileUploadIcon />
+            </Button>
           </Grid>
         </Grid>
-      </Grid>
+      </Card>
     </>
   );
 };
