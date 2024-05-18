375b5d8d89d331aaf2461a600971edbfa174c325
aditikashyap914 <aditikashyap914@gmail.com>
2024-05-18 12:23:31 +0530

    AboutUs updated...

--- client/src/components/AboutUs/aboutUs.js
+++ client/src/components/AboutUs/aboutUs.js
@@ -121,15 +121,17 @@ const AboutUs = () => {
           </Typography>{" "}
           <p style={{ fontFamily: "Roboto" }}>
             Our business ethics and high professional integrity drive us to
-            achieve excellence in engineering with values such as - Trust - The
-            organization functions with the belief that everyone embraces the
-            value of the company to deliver the best for the client. Empowerment
-            - We believe in an environment that enables our team to make the
-            decision that contributes to our client's delight in getting
-            world-class engineering solutions. Business Ethics - It's our moral
-            responsibility to adopt robust business ethics to serve our clients
-            with leadership, Integrity, and Accountability. We continuously
-            challenge ourselves to achieve high business ethics.
+            achieve excellence in engineering with values such as - <br />
+            Trust - The organization functions with the belief that everyone
+            embraces the value of the company to deliver the best for the
+            client. <br />
+            Empowerment - We believe in an environment that enables our team to
+            make the decision that contributes to our client's delight in
+            getting world-class engineering solutions. <br />
+            Business Ethics - It's our moral responsibility to adopt robust
+            business ethics to serve our clients with leadership, Integrity, and
+            Accountability. We continuously challenge ourselves to achieve high
+            business ethics.
           </p>
           <br />
           <p style={{ fontStyle: "oblique", fontFamily: "Roboto" }}>
