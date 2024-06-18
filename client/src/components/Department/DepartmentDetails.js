5513995981c1f1e2e4399c805fd7cbd46094ee4b
aditikashyap914 <aditikashyap914@gmail.com>
2024-06-18 17:36:38 +0530

    organogram Layout started need some modification

--- client/src/components/Department/DepartmentDetails.js
+++ client/src/components/Department/DepartmentDetails.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { styled } from "@mui/material/styles";
 import {
   Container,
   Card,
@@ -6,51 +7,93 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Box,
+  Grid,
+  Paper,
 } from "@mui/material";
+import avatar1 from "../../assets/MD.jpg";
 
 const DepartmentDetails = () => {
   const user = JSON.parse(localStorage.getItem("profile"));
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "grey",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.primary,
+  }));
+
   return (
-    <div>
-      <Container
-        fluid="true"
-        sx={{
-          padding: "20px",
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
-        }}
-      >
-        <Card
-          elevation={20}
+    <div style={{ display: "flex", flex: 2 }}>
+      <Grid sx={{ flexDirection: "column", padding: "10px" }}>
+        <Grid
           sx={{
-            padding: "10px",
-            borderRadius: "5px",
-            display: {
-              xs: "0",
-              sm: "600",
-            },
-            bgcolor: "background.Card",
-            boxShadow: "5px",
-            width: "auto",
-            justifyContent: "center",
+            display: "flex",
+            flexDirection: "row",
+            backgroundColor: "pink",
           }}
         >
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={user.result.selectedFile}
-              alt="green iguana"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Lizard
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </Container>
+          <Grid>
+            <CardActionArea>
+              <CardMedia
+                component="img"
+                height="100px"
+                image={avatar1}
+                alt="green iguana"
+              />
+            </CardActionArea>
+          </Grid>
+
+          <Grid>
+            <Typography
+              component="div"
+              sx={{
+                fontSize: "15px",
+                marginTop: "30px",
+                marginLeft: "10px",
+                fontWeight: "bolder",
+              }}
+            >
+              MR. ABHISHEK KUMAR
+            </Typography>
+
+            <Typography
+              sx={{
+                fontSize: "15px",
+                marginLeft: "10px",
+              }}
+            >
+              Managing <br />
+              Director
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid sx={{ marginTop: "20px" }}>
+          <Grid
+            container
+            justifyContent="space-evenly"
+            spacing={1}
+            sx={{ display: "flex", flexDirection: "row" }}
+          >
+            <Grid item>
+              <Item>ENGINEERING</Item>
+            </Grid>
+            <Grid item xs={3}>
+              <Item>IT</Item>
+            </Grid>
+            <Grid item>
+              <Item>HR & ADMIN</Item>
+            </Grid>
+            <Grid item>
+              <Item>ACCOUNTS & TAXATION</Item>
+            </Grid>
+            <Grid item>
+              <Item>COMMERCIAL & BD</Item>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
