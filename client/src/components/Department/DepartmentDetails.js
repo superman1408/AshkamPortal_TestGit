934114ca237df7173b6d13304d4c955f9771c28b
aditikashyap914 <aditikashyap914@gmail.com>
2024-06-19 17:29:19 +0530

    Layout updated in departmentDetails module

--- client/src/components/Department/DepartmentDetails.js
+++ client/src/components/Department/DepartmentDetails.js
@@ -25,65 +25,23 @@ const DepartmentDetails = () => {
   }));
 
   return (
-    <div style={{ display: "flex", flex: 2 }}>
+    <div style={{ display: "flex", flex: 1 }}>
       <Grid sx={{ flexDirection: "column", padding: "10px" }}>
-        <Grid
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            backgroundColor: "pink",
-          }}
-        >
-          <Grid>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                height="100px"
-                image={avatar1}
-                alt="green iguana"
-              />
-            </CardActionArea>
-          </Grid>
-
-          <Grid>
-            <Typography
-              component="div"
-              sx={{
-                fontSize: "15px",
-                marginTop: "30px",
-                marginLeft: "10px",
-                fontWeight: "bolder",
-              }}
-            >
-              MR. ABHISHEK KUMAR
-            </Typography>
-
-            <Typography
-              sx={{
-                fontSize: "15px",
-                marginLeft: "10px",
-              }}
-            >
-              Managing <br />
-              Director
-            </Typography>
-          </Grid>
-        </Grid>
         <Grid sx={{ marginTop: "20px" }}>
           <Grid
             container
-            justifyContent="space-evenly"
-            spacing={1}
-            sx={{ display: "flex", flexDirection: "row" }}
+            // justifyContent="space-evenly"
+            spacing={7}
+            sx={{ display: "flex", flexDirection: "row", marginLeft: "60px" }}
           >
             <Grid item>
               <Item>ENGINEERING</Item>
             </Grid>
-            <Grid item xs={3}>
-              <Item>IT</Item>
+            <Grid item>
+              <Item>INFORMATION TECHNOLOGY</Item>
             </Grid>
             <Grid item>
-              <Item>HR & ADMIN</Item>
+              <Item>HUMAN RESOURCE </Item>
             </Grid>
             <Grid item>
               <Item>ACCOUNTS & TAXATION</Item>
