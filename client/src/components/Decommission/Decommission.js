d95f885b838d3bdf4dfd09e63d897fefe467f7e9
superman1408 <superman14082017@gmail.com>
2024-07-03 13:19:34 +0530

    Updating

--- client/src/components/Decommission/Decommission.js
+++ client/src/components/Decommission/Decommission.js
@@ -4,14 +4,16 @@ import video1 from "../../assets/DECOMMISSION.mp4";
 
 const Decommission = () => {
   return (
+    <Container sx={{justifyContent: "center"}}>
     <Card
       elevation={10}
-      sx={{ display: "flex", width: "1300px", borderRadius: "12px" }}
+      sx={{ display: "flex", width: "1300px", borderRadius: "12px",justifyContent: "center" }}
     >
       <video loop autoPlay muted style={{ display: "flex", width: "1300px" }}>
         <source src={video1} type="video/mp4" />
       </video>
     </Card>
+    </Container>
   );
 };
 
