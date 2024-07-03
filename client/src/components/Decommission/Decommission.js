5d89d2830b4def35de432911251409c9ef9c07fb
superman1408 <superman14082017@gmail.com>
2024-07-03 13:32:51 +0530

    aligned properly

--- client/src/components/Decommission/Decommission.js
+++ client/src/components/Decommission/Decommission.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import { Card, Container } from "@mui/material";
 import video1 from "../../assets/DECOMMISSION.mp4";
 
 const Decommission = () => {
+
+  const [dimension, setDimension] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const detectSize = () => {
+    setDimension({ width: window.innerWidth, height: window.innerHeight });
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", detectSize, { passive: true });
+    return () => {
+      window.removeEventListener("resize", detectSize, { passive: true });
+    };
+  }, [dimension]);
   return (
-    <Container sx={{justifyContent: "center"}}>
+    <Container sx={{
+      // padding: "20px",
+      display: "flex",
+      justifyContent: "center",
+      marginBottom: "20px",
+    }}>
     <Card
       elevation={10}
-      sx={{ display: "flex", width: "1300px", borderRadius: "12px",justifyContent: "center" }}
+      sx={{ display: "flex", width: "1300px", borderRadius: "30px",justifyContent: "center" }}
     >
-      <video loop autoPlay muted style={{ display: "flex", width: "1300px" }}>
+      <video loop autoPlay muted style={{ display: "flex", width: "1200px" }}>
         <source src={video1} type="video/mp4" />
       </video>
     </Card>
