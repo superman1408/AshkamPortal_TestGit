b10639e78a0b5aa00ce63cf5a8ecf03f008a94d3
Farheen Kaifee <farheenkaifee555@gmail.com>
2025-10-07 11:55:10 +0530

    Some bugs are rectified & page inside birthday os also corrected

--- client/src/components/Department/DepartmentDetails.js
+++ client/src/components/Department/DepartmentDetails.js
@@ -1,24 +1,256 @@
+// import React, { useEffect } from "react";
+// import { useNavigate } from "react-router-dom";
+// import { useDispatch, useSelector } from "react-redux";
+// import { getPosts } from "../../action/posts";
+// import { styled } from "@mui/material/styles";
+// import SouthIcon from "@mui/icons-material/South";
+// import {
+//   Container,
+//   Card,
+//   CardActionArea,
+//   CardMedia,
+//   CardContent,
+//   Typography,
+//   Box,
+//   Grid,
+//   Paper,
+//   IconButton,
+//   Button,
+//   CardActions,
+//   ButtonBase,
+//   Divider,
+// } from "@mui/material";
+// import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+
+// const DepartmentDetails = () => {
+//   const user = JSON.parse(localStorage.getItem("profile"));
+//   const posts = useSelector((state) => state.posts);
+//   const verifyDepat = user.result.department;
+
+//   const dispatch = useDispatch();
+
+//   const navigate = useNavigate();
+
+//   const Item = styled(Paper)(({ theme }) => ({
+//     backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "grey",
+//     ...theme.typography.body2,
+//     padding: theme.spacing(1),
+//     textAlign: "center",
+//     color: theme.palette.text.primary,
+//   }));
+
+//   useEffect(() => {
+//     if (posts) {
+//       dispatch(getPosts());
+//     }
+//   }, [dispatch, posts]);
+
+//   const handleGoBack = () => {
+//     navigate(-1);
+//   };
+
+//   return (
+//     <>
+//       <div style={{ display: "inline", float: "left" }}>
+//         <Button
+//           onClick={handleGoBack}
+//           sx={{
+//             padding: "8px 16px",
+//             color: "#16355d",
+//             float: "left",
+//             display: {
+//               xs: "none",
+//               sm: "inline-block",
+//             },
+//           }}
+//         >
+//           <ArrowBackIcon />
+//         </Button>
+//       </div>
+//       <Container
+//         fluid="true"
+//         sx={{
+//           padding: "10px",
+//           display: "flex",
+//           justifyContent: "center",
+//           marginTop: "20px",
+//         }}
+//       >
+//         <Grid sx={{ textAlign: "center" }}>
+//           <Typography
+//             variant="h4"
+//             sx={{ color: "#16355d", fontFamily: "Roboto" }}
+//           >
+//             {verifyDepat}
+//           </Typography>
+//           <Grid>
+//             <div
+//               style={{
+//                 display: "flex",
+//                 flexWrap: "wrap",
+//                 justifyContent: "center",
+//               }}
+//             >
+//               {posts.map((post) => {
+//                 if (
+//                   post.department === verifyDepat &&
+//                   post.role === "manager"
+//                 ) {
+//                   return (
+//                     <div style={{ margin: "20px" }}>
+//                       <ButtonBase>
+//                         <Card
+//                           sx={{
+//                             width: 180,
+//                             maxHeight: 220,
+//                             borderRadius: "10%",
+//                           }}
+//                         >
+//                           <CardMedia
+//                             component="img"
+//                             image={post.selectedFile}
+//                             alt="Post image"
+//                             sx={{
+//                               padding: "2px",
+//                               height: "150px",
+//                               borderRadius: "10%", // or "50%" for circle
+//                               objectFit: "cover",
+//                             }}
+//                           />
+//                           <CardContent sx={{ padding: "5px" }}>
+//                             <Typography
+//                               gutterBottom
+//                               component="div"
+//                               sx={{
+//                                 fontSize: "18px",
+//                                 color: "#16355d",
+//                                 fontFamily: "Roboto",
+//                                 lineHeight: 1.2,
+//                               }}
+//                             >
+//                               {post.firstName + " " + post.lastName}
+//                             </Typography>
+//                             <Typography
+//                               sx={{
+//                                 fontSize: "15px",
+//                                 color: "#16355d",
+//                                 fontFamily: "Roboto",
+//                                 lineHeight: 1.2,
+//                               }}
+//                             >
+//                               {post.jobTitle}
+//                             </Typography>
+//                           </CardContent>
+//                         </Card>
+//                       </ButtonBase>
+//                     </div>
+//                   );
+//                 }
+//               })}
+//             </div>
+//           </Grid>
+//           <Divider
+//             sx={{
+//               borderWidth: "5px",
+//               bgcolor: "grey",
+//             }}
+//           />
+//           <Grid sx={{ marginTop: "20px", textAlign: "center" }}>
+//             <div
+//               style={{
+//                 display: "flex",
+//                 flexWrap: "wrap",
+//                 justifyContent: "center",
+//               }}
+//             >
+//               {posts
+//                 .sort((a, b) =>
+//                   (a.firstName + " " + a.lastName).localeCompare(
+//                     b.firstName + " " + b.lastName
+//                   )
+//                 )
+//                 .map((post, index) => {
+//                   if (
+//                     post.department === verifyDepat &&
+//                     post.role === "employee"
+//                   ) {
+//                     return (
+//                       <div key={index} style={{ margin: "20px" }}>
+//                         <ButtonBase>
+//                           <Card
+//                             sx={{
+//                               width: 180,
+//                               maxHeight: 220,
+//                               borderRadius: "10%",
+//                             }}
+//                           >
+//                             <CardMedia
+//                               component="img"
+//                               image={post.selectedFile}
+//                               alt="Post image"
+//                               sx={{
+//                                 padding: "2px",
+//                                 height: "150px",
+//                                 borderRadius: "10%", // or "50%" for circle
+//                                 objectFit: "cover",
+//                               }}
+//                             />
+
+//                             <CardContent sx={{ padding: "5px" }}>
+//                               <Typography
+//                                 gutterBottom
+//                                 component="div"
+//                                 sx={{
+//                                   fontSize: "18px",
+//                                   color: "#16355d",
+//                                   fontFamily: "Roboto",
+//                                   lineHeight: 1.2,
+//                                 }}
+//                               >
+//                                 {post.firstName + " " + post.lastName}
+//                               </Typography>
+//                               <Typography
+//                                 sx={{
+//                                   mb: "2px",
+//                                   fontSize: "15px",
+//                                   color: "#16355d",
+//                                   fontFamily: "Roboto",
+//                                   lineHeight: 1.2,
+//                                 }}
+//                               >
+//                                 {post.jobTitle}
+//                               </Typography>
+//                             </CardContent>
+//                           </Card>
+//                         </ButtonBase>
+//                       </div>
+//                     );
+//                   }
+//                 })}
+//             </div>
+//           </Grid>
+//         </Grid>
+//       </Container>
+//     </>
+//   );
+// };
+
+// export default DepartmentDetails;
+
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getPosts } from "../../action/posts";
-import { styled } from "@mui/material/styles";
-import SouthIcon from "@mui/icons-material/South";
 import {
   Container,
   Card,
-  CardActionArea,
   CardMedia,
   CardContent,
   Typography,
-  Box,
   Grid,
-  Paper,
   IconButton,
-  Button,
-  CardActions,
-  ButtonBase,
   Divider,
+  Box,
 } from "@mui/material";
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
@@ -28,16 +260,7 @@ const DepartmentDetails = () => {
   const verifyDepat = user.result.department;
 
   const dispatch = useDispatch();
-
   const navigate = useNavigate();
-
-  const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "grey",
-    ...theme.typography.body2,
-    padding: theme.spacing(1),
-    textAlign: "center",
-    color: theme.palette.text.primary,
-  }));
 
   useEffect(() => {
     if (posts) {
@@ -49,189 +272,115 @@ const DepartmentDetails = () => {
     navigate(-1);
   };
 
-  return (
-    <>
-      <div style={{ display: "inline", float: "left" }}>
-        <Button
-          onClick={handleGoBack}
-          sx={{
-            padding: "8px 16px",
-            color: "#16355d",
-            float: "left",
-            display: {
-              xs: "none",
-              sm: "inline-block",
-            },
-          }}
-        >
-          <ArrowBackIcon />
-        </Button>
-      </div>
-      <Container
-        fluid="true"
+  const renderCard = (post) => (
+    <Card
+      sx={{
+        width: 200,
+        borderRadius: 4,
+        overflow: "hidden",
+        boxShadow: 3,
+        transition: "transform 0.2s ease, box-shadow 0.2s ease",
+        "&:hover": {
+          transform: "translateY(-5px)",
+          boxShadow: 6,
+        },
+      }}
+    >
+      <CardMedia
+        component="img"
+        image={post.selectedFile}
+        alt="Profile"
         sx={{
-          padding: "10px",
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "20px",
+          height: 160,
+          objectFit: "cover",
+        }}
+      />
+      <CardContent sx={{ textAlign: "center", p: 2 }}>
+        <Typography
+          variant="subtitle1"
+          sx={{ fontWeight: 600, color: "#16355d" }}
+        >
+          {post.firstName + " " + post.lastName}
+        </Typography>
+        <Typography variant="body2" sx={{ color: "text.secondary" }}>
+          {post.jobTitle}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+
+  return (
+    <Container maxWidth="lg" sx={{ mt: 4, mb: 6, position: "relative" }}>
+      {/* Back Button */}
+      <IconButton
+        onClick={handleGoBack}
+        sx={{
+          position: "absolute",
+          left: 16,
+          top: 16,
+          zIndex: 1000, // ensures it’s on top
+          "&:hover": { bgcolor: "#e3eaf3" },
         }}
       >
-        <Grid sx={{ textAlign: "center" }}>
-          <Typography
-            variant="h4"
-            sx={{ color: "#16355d", fontFamily: "Roboto" }}
-          >
-            {verifyDepat}
-          </Typography>
-          <Grid>
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "center",
-              }}
-            >
-              {posts.map((post) => {
-                if (
-                  post.department === verifyDepat &&
-                  post.role === "manager"
-                ) {
-                  return (
-                    <div style={{ margin: "20px" }}>
-                      <ButtonBase>
-                        <Card
-                          sx={{
-                            width: 180,
-                            maxHeight: 220,
-                            borderRadius: "10%",
-                          }}
-                        >
-                          <CardMedia
-                            component="img"
-                            image={post.selectedFile}
-                            alt="Post image"
-                            sx={{
-                              padding: "2px",
-                              height: "150px",
-                              borderRadius: "10%", // or "50%" for circle
-                              objectFit: "cover",
-                            }}
-                          />
-                          <CardContent sx={{ padding: "5px" }}>
-                            <Typography
-                              gutterBottom
-                              component="div"
-                              sx={{
-                                fontSize: "18px",
-                                color: "#16355d",
-                                fontFamily: "Roboto",
-                                lineHeight: 1.2,
-                              }}
-                            >
-                              {post.firstName + " " + post.lastName}
-                            </Typography>
-                            <Typography
-                              sx={{
-                                fontSize: "15px",
-                                color: "#16355d",
-                                fontFamily: "Roboto",
-                                lineHeight: 1.2,
-                              }}
-                            >
-                              {post.jobTitle}
-                            </Typography>
-                          </CardContent>
-                        </Card>
-                      </ButtonBase>
-                    </div>
-                  );
-                }
-              })}
-            </div>
-          </Grid>
-          <Divider
-            sx={{
-              borderWidth: "5px",
-              bgcolor: "grey",
-            }}
-          />
-          <Grid sx={{ marginTop: "20px", textAlign: "center" }}>
-            <div
-              style={{
-                display: "flex",
-                flexWrap: "wrap",
-                justifyContent: "center",
-              }}
-            >
-              {posts
-                .sort((a, b) =>
-                  (a.firstName + " " + a.lastName).localeCompare(
-                    b.firstName + " " + b.lastName
-                  )
-                )
-                .map((post, index) => {
-                  if (
-                    post.department === verifyDepat &&
-                    post.role === "employee"
-                  ) {
-                    return (
-                      <div key={index} style={{ margin: "20px" }}>
-                        <ButtonBase>
-                          <Card
-                            sx={{
-                              width: 180,
-                              maxHeight: 220,
-                              borderRadius: "10%",
-                            }}
-                          >
-                            <CardMedia
-                              component="img"
-                              image={post.selectedFile}
-                              alt="Post image"
-                              sx={{
-                                padding: "2px",
-                                height: "150px",
-                                borderRadius: "10%", // or "50%" for circle
-                                objectFit: "cover",
-                              }}
-                            />
+        <ArrowBackIcon sx={{ color: "#16355d" }} />
+      </IconButton>
+      {/* Department Title */}
+      <Box textAlign="center" mb={4}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          sx={{ color: "#16355d", fontFamily: "Roboto" }}
+        >
+          {verifyDepat}
+        </Typography>
+      </Box>
 
-                            <CardContent sx={{ padding: "5px" }}>
-                              <Typography
-                                gutterBottom
-                                component="div"
-                                sx={{
-                                  fontSize: "18px",
-                                  color: "#16355d",
-                                  fontFamily: "Roboto",
-                                  lineHeight: 1.2,
-                                }}
-                              >
-                                {post.firstName + " " + post.lastName}
-                              </Typography>
-                              <Typography
-                                sx={{
-                                  mb: "2px",
-                                  fontSize: "15px",
-                                  color: "#16355d",
-                                  fontFamily: "Roboto",
-                                  lineHeight: 1.2,
-                                }}
-                              >
-                                {post.jobTitle}
-                              </Typography>
-                            </CardContent>
-                          </Card>
-                        </ButtonBase>
-                      </div>
-                    );
-                  }
-                })}
-            </div>
-          </Grid>
+      {/* Managers */}
+      <Box mb={3}>
+        <Grid container spacing={3} justifyContent="center">
+          {posts
+            .filter(
+              (post) =>
+                post.department === verifyDepat && post.role === "manager"
+            )
+            .map((post, idx) => (
+              <Grid item key={idx}>
+                {renderCard(post)}
+              </Grid>
+            ))}
         </Grid>
-      </Container>
-    </>
+      </Box>
+
+      <Divider
+        sx={{
+          marginTop: "20px",
+          marginBottom: "20px",
+          borderWidth: "5px",
+          bgcolor: "#336699",
+        }}
+      />
+
+      {/* Employees */}
+      <Box>
+        <Grid container spacing={3} justifyContent="center">
+          {posts
+            .filter(
+              (post) =>
+                post.department === verifyDepat && post.role === "employee"
+            )
+            .sort((a, b) =>
+              (a.firstName + " " + a.lastName).localeCompare(
+                b.firstName + " " + b.lastName
+              )
+            )
+            .map((post, idx) => (
+              <Grid item key={idx}>
+                {renderCard(post)}
+              </Grid>
+            ))}
+        </Grid>
+      </Box>
+    </Container>
   );
 };
 
